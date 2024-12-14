a968891d5b3c7455163a95b75b2b8a1257e4d7bd
Jeevan Sanchez <jeevansanchez42@gmail.com>
2024-12-14 17:41:26 -0500

    done

--- src/components/health/ChatBot.tsx
+++ src/components/health/ChatBot.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const wow = wow
+
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "the content you will be receiving is the latest values for heart rate, blood pressure, and respiratory rate. Based on these you should give professional healthcare advice including whether these values are in the healthy range, and then give advice such as diet improvements ." 
